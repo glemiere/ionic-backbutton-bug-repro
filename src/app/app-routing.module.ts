7cb601537cafac36f9f9c5dd41ed0dd2eed41f87
glemiere <guillaumelm.in@gmail.com>
2023-01-05 00:42:07 -0700

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,10 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'page',
+    loadChildren: () => import('./page/page.module').then( m => m.PagePageModule)
   }
 ];
 @NgModule({
